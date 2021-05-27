7ba8da12a4521f550d1a2bb7acad8ced7d2ff1ac
dove <ndoverte11@gmail.com>
2021-05-27 18:59:39 +0800

    start coding the footer

---
+++ src/containers/TheFooter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TheFooter = () => {
+  return (
+    <Footer>
+      <h1>The Footer</h1>
+    </Footer>
+  );
+};
+
+export default TheFooter;
+<h1>The Footer</h1>;
